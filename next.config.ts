32e1e2092e4a245f78533b02e98d399b9c2f66fd
Pranay Nandiwadekar <142014316+pranayndv@users.noreply.github.com>
2025-11-21 09:54:04 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "m.media-amazon.com",           
+    ],
+  },
+};
+
+export default nextConfig;
